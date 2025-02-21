e2fe3ceb42aeb52a8b9effa04f7ffea78fa52f53
jackyliu1 <90361783+jackyliu1@users.noreply.github.com>
2025-02-21 14:55:19 -0500

    Update a1q2.c

--- a1q2.c
+++ a1q2.c
@@ -1,28 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-
+    
 #define BUFSIZE 256
-
+    
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    if (argc != 2) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-
-    FILE* file = fopen(argv[1], "r");
-    if (!file) {
-        fprintf(stderr, "Error: the file can't be open\n", argv[1]);
-        return -1;
-    }
-    fclose(file);
-
-    char cmd[BUFSIZE];
-    int limited = snprintf(cmd, BUFSIZE, "wc -c < \"%s\"", argv[1]);
-    if (limited < 0 || limited >= BUFSIZE) {
-        fprintf(stderr, "Error: the file path is too long.\n");
-        return -1;
-    }
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[1]);
     system(cmd);
 }
